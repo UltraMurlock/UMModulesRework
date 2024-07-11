73d296fcd3517cba64f51bb89995217ed895601e
UltraMurlock <egorplettnev00@gmail.com>
2024-07-11 11:12:40 +0300

    Рефакторинг
    Переменная level переименована в tier по всему проекту

--- scripts/base-modules.lua
+++ scripts/base-modules.lua
@@ -1,48 +1,48 @@
-function UpdateSpeedModule(name, level)
-	data.raw["module"][name].effect.speed = { bonus = Round(SmSpeedStart * SmSpeedMult ^ (level - 1)) }
+function UpdateSpeedModule(name, tier)
+	data.raw["module"][name].effect.speed = { bonus = Round(SmSpeedStart * SmSpeedMult ^ (tier - 1)) }
 
 	if(SmConsumptionPenalty) then
-		data.raw["module"][name].effect.consumption = { bonus = Round(SmConsumptionStart * SmConsumptionMult ^ (level - 1)) }
+		data.raw["module"][name].effect.consumption = { bonus = Round(SmConsumptionStart * SmConsumptionMult ^ (tier - 1)) }
 	else
 		data.raw["module"][name].effect.consumption = nil
 	end
 
 	if(SmPollutionPenalty) then
-		data.raw["module"][name].effect.pollution = { bonus = Round(SmPollutionStart * SmPollutionMult ^ (level - 1)) }
+		data.raw["module"][name].effect.pollution = { bonus = Round(SmPollutionStart * SmPollutionMult ^ (tier - 1)) }
 	else
 		data.raw["module"][name].effect.pollution = nil
 	end
 end
 
 
-function UpdateEffectivityModule(name, level)
-	data.raw["module"][name].effect.consumption = { bonus = -Round(EmEffectivityStart * EmEffectivityMult ^ (level - 1)) }
+function UpdateEffectivityModule(name, tier)
+	data.raw["module"][name].effect.consumption = { bonus = -Round(EmEffectivityStart * EmEffectivityMult ^ (tier - 1)) }
 
 	if(EmPollutionBonus) then
-		data.raw["module"][name].effect.pollution = { bonus = -Round(EmPolutionStart * EmPolutionMult ^ (level - 1)) }
+		data.raw["module"][name].effect.pollution = { bonus = -Round(EmPolutionStart * EmPolutionMult ^ (tier - 1)) }
 	else
 		data.raw["module"][name].effect.pollution = nil
 	end
 end
 
 
-function UpdateProductivityModule(name, level)
-	data.raw["module"][name].effect.productivity = { bonus = Round(PmProductivityStart * PmProductivityMult ^ (level - 1)) }
+function UpdateProductivityModule(name, tier)
+	data.raw["module"][name].effect.productivity = { bonus = Round(PmProductivityStart * PmProductivityMult ^ (tier - 1)) }
 
 	if(PmConsumptionPenalty) then
-		data.raw["module"][name].effect.consumption = { bonus = Round(PmConsumptionStart * PmConsumptionMult ^ (level - 1)) }
+		data.raw["module"][name].effect.consumption = { bonus = Round(PmConsumptionStart * PmConsumptionMult ^ (tier - 1)) }
 	else
 		data.raw["module"][name].effect.consumption = nil
 	end
 
 	if(PmSpeedPenalty) then
-		data.raw["module"][name].effect.speed = { bonus = -Round(PmSpeedStart * PmSpeedMult ^ (level - 1)) }
+		data.raw["module"][name].effect.speed = { bonus = -Round(PmSpeedStart * PmSpeedMult ^ (tier - 1)) }
 	else
 		data.raw["module"][name].effect.speed = nil
 	end
 
 	if(PmPollutionPenalty) then
-		data.raw["module"][name].effect.pollution = { bonus = Round(PmPollutionStart * PmPollutionMult ^ (level - 1)) }
+		data.raw["module"][name].effect.pollution = { bonus = Round(PmPollutionStart * PmPollutionMult ^ (tier - 1)) }
 	else
 		data.raw["module"][name].effect.pollution = nil
 	end
